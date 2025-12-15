170b67efa6b2ee8914068c72a5518828e0188740
Vitaly Turovsky <vital2580@icloud.com>
2025-12-15 22:58:14 +0300

    add redirect default

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,26 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { useEffect } from 'react'
 import Home from './components/Home'
 import Ow from './components/Ow'
 import { PuffsContainer } from './components/Background'
 import Typescript from './Typescript'
 import NotFound from './NotFound'
 import Bubbles from './components/Bubbles'
+
+const RedirectToR = () => {
+    useEffect(() => {
+        const { hostname, pathname, search, hash } = window.location
+        const targetHost = 'r.zardoy.com'
+
+        // Only redirect if we're on zardoy.com (not already on r.zardoy.com)
+        if (hostname === 'zardoy.com' || hostname === 'www.zardoy.com') {
+            const newUrl = `https://${targetHost}${pathname}${search}${hash}`
+            window.location.replace(newUrl)
+        }
+    }, [])
+
+    return null
+}
 
 const router = createBrowserRouter([
     {
@@ -24,8 +40,16 @@ const router = createBrowserRouter([
         element: <Bubbles />,
     },
     {
-        path: '/*',
+        path: '/404',
         element: <NotFound />,
+    },
+    {
+        path: '/test',
+        element: <NotFound />,
+    },
+    {
+        path: '/*',
+        element: <RedirectToR />,
     },
 ])
 
